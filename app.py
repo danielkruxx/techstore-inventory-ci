235ad0a43efdeadfa2bcf13f9cd71e0385ca80a1
Jose Daniel Cruz Lopez <josedaniel@techstore.local>
2026-09-25 15:49:22 -0600

    temp: revert to vulnerable app to capture E6

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 
-import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = "TechStore123!"
 
-@app.route("/")
+@app.get("/")
 def home():
     return jsonify(app="InventoryHub", version="2.0.0")
 
@@ -14,4 +13,4 @@ def health():
     return jsonify(status="ok"), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
